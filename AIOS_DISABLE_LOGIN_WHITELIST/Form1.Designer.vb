b7a48f6d4e045fa45ee121ed0d8c60e5f328d40e
makpapad <makpapad@gmail.com>
2024-10-23 09:31:03 +0300

    Πρώτο commit του project

---
+++ AIOS_DISABLE_LOGIN_WHITELIST/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        TextBoxHost = New TextBox()
+        TextBoxUsername = New TextBox()
+        TextBoxFilePath = New TextBox()
+        RichTextBoxPrivateKey = New RichTextBox()
+        LabelCurrentValue = New Label()
+        ButtonEdit = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(55, 320)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "Send"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBoxHost
+        ' 
+        TextBoxHost.Location = New Point(55, 46)
+        TextBoxHost.Name = "TextBoxHost"
+        TextBoxHost.ReadOnly = True
+        TextBoxHost.Size = New Size(150, 31)
+        TextBoxHost.TabIndex = 1
+        TextBoxHost.Text = "Host"
+        ' 
+        ' TextBoxUsername
+        ' 
+        TextBoxUsername.Location = New Point(55, 83)
+        TextBoxUsername.Name = "TextBoxUsername"
+        TextBoxUsername.ReadOnly = True
+        TextBoxUsername.Size = New Size(150, 31)
+        TextBoxUsername.TabIndex = 2
+        TextBoxUsername.Text = "User Name"
+        ' 
+        ' TextBoxFilePath
+        ' 
+        TextBoxFilePath.Location = New Point(55, 120)
+        TextBoxFilePath.Name = "TextBoxFilePath"
+        TextBoxFilePath.ReadOnly = True
+        TextBoxFilePath.Size = New Size(150, 31)
+        TextBoxFilePath.TabIndex = 3
+        TextBoxFilePath.Text = "Path"
+        ' 
+        ' RichTextBoxPrivateKey
+        ' 
+        RichTextBoxPrivateKey.Location = New Point(264, 33)
+        RichTextBoxPrivateKey.Name = "RichTextBoxPrivateKey"
+        RichTextBoxPrivateKey.ReadOnly = True
+        RichTextBoxPrivateKey.Size = New Size(467, 321)
+        RichTextBoxPrivateKey.TabIndex = 4
+        RichTextBoxPrivateKey.Text = "Private Key"
+        ' 
+        ' LabelCurrentValue
+        ' 
+        LabelCurrentValue.AutoSize = True
+        LabelCurrentValue.Font = New Font("Segoe UI", 14F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        LabelCurrentValue.Location = New Point(264, 387)
+        LabelCurrentValue.Name = "LabelCurrentValue"
+        LabelCurrentValue.Size = New Size(105, 38)
+        LabelCurrentValue.TabIndex = 5
+        LabelCurrentValue.Text = "Label1"
+        ' 
+        ' ButtonEdit
+        ' 
+        ButtonEdit.Location = New Point(55, 176)
+        ButtonEdit.Name = "ButtonEdit"
+        ButtonEdit.Size = New Size(112, 34)
+        ButtonEdit.TabIndex = 6
+        ButtonEdit.Text = "Edit"
+        ButtonEdit.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ButtonEdit)
+        Controls.Add(LabelCurrentValue)
+        Controls.Add(RichTextBoxPrivateKey)
+        Controls.Add(TextBoxFilePath)
+        Controls.Add(TextBoxUsername)
+        Controls.Add(TextBoxHost)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "AIOS_DISABLE_LOGIN_WHITELIST"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBoxHost As TextBox
+    Friend WithEvents TextBoxUsername As TextBox
+    Friend WithEvents TextBoxFilePath As TextBox
+    Friend WithEvents RichTextBoxPrivateKey As RichTextBox
+    Friend WithEvents LabelCurrentValue As Label
+    Friend WithEvents ButtonEdit As Button
+
+End Class
---
+++ AIOS_DISABLE_LOGIN_WHITELIST/Form1.vb
@@ -0,0 +1,169 @@
+﻿Imports Renci.SshNet
+Imports System.IO
+
+Public Class Form1
+    ' Αρχείο αποθήκευσης ρυθμίσεων
+    Private settingsFilePath As String = "settings.enc"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Αρχικά όλα τα πεδία είναι μη εγγράψιμα
+        SetFieldsReadOnly(True)
+
+        ' Φόρτωση των αποθηκευμένων ρυθμίσεων κατά την εκκίνηση
+        LoadSettings()
+
+        ' Εμφάνιση της τρέχουσας τιμής του AIOS_DISABLE_LOGIN_WHITELIST αν υπάρχουν αποθηκευμένες ρυθμίσεις
+        If Not String.IsNullOrEmpty(TextBoxHost.Text) AndAlso Not String.IsNullOrEmpty(TextBoxFilePath.Text) Then
+            ShowCurrentSettingValue(TextBoxHost.Text, TextBoxUsername.Text, RichTextBoxPrivateKey.Text, TextBoxFilePath.Text)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Πάρε τις πληροφορίες από τα πεδία
+        Dim host As String = TextBoxHost.Text
+        Dim username As String = TextBoxUsername.Text
+        Dim privateKey As String = RichTextBoxPrivateKey.Text
+        Dim filePath As String = TextBoxFilePath.Text
+
+        ' Αποθήκευση των πληροφοριών κωδικοποιημένων
+        SaveSettings(host, username, privateKey, filePath)
+
+        ' Εκτέλεση της αλλαγής στο αρχείο wp-config.php
+        Try
+            ModifyWpConfig(host, username, privateKey, filePath)
+            ' Ενημέρωση της τρέχουσας τιμής μετά την αλλαγή
+            ShowCurrentSettingValue(host, username, privateKey, filePath)
+            MessageBox.Show("Η αλλαγή ολοκληρώθηκε με επιτυχία.")
+        Catch ex As Exception
+            MessageBox.Show("Αποτυχία σύνδεσης: " & ex.Message)
+        End Try
+
+        ' Κάνε τα πεδία ξανά μη εγγράψιμα
+        SetFieldsReadOnly(True)
+    End Sub
+
+    Private Sub ButtonEdit_Click(sender As Object, e As EventArgs) Handles ButtonEdit.Click
+        ' Κάνε τα πεδία εγγράψιμα
+        SetFieldsReadOnly(False)
+    End Sub
+
+    Private Sub SetFieldsReadOnly(isReadOnly As Boolean)
+        ' Ορισμός της κατάστασης εγγραφής των πεδίων
+        TextBoxHost.ReadOnly = isReadOnly
+        TextBoxUsername.ReadOnly = isReadOnly
+        RichTextBoxPrivateKey.ReadOnly = isReadOnly
+        TextBoxFilePath.ReadOnly = isReadOnly
+    End Sub
+
+    Private Sub SaveSettings(host As String, username As String, privateKey As String, filePath As String)
+        ' Συνένωση των δεδομένων σε ένα string, διαχωρισμένα με ένα ξεχωριστό σύμβολο
+        Dim data As String = $"{host}|||{username}|||{privateKey}|||{filePath}"
+
+        ' Κωδικοποίηση σε Base64
+        Dim encodedData As String = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(data))
+
+        ' Αποθήκευση σε αρχείο
+        File.WriteAllText(settingsFilePath, encodedData)
+    End Sub
+
+    Private Sub LoadSettings()
+        ' Έλεγχος αν το αρχείο υπάρχει
+        If Not File.Exists(settingsFilePath) Then
+            Return ' Αν δεν υπάρχει, δεν κάνουμε τίποτα
+        End If
+
+        ' Ανάγνωση και αποκωδικοποίηση των δεδομένων από το αρχείο
+        Dim encodedData As String = File.ReadAllText(settingsFilePath)
+        Dim decodedData As String = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(encodedData))
+
+        ' Διαχωρισμός των δεδομένων
+        Dim parts As String() = decodedData.Split(New String() {"|||"}, StringSplitOptions.None)
+        If parts.Length = 4 Then
+            TextBoxHost.Text = parts(0)
+            TextBoxUsername.Text = parts(1)
+            RichTextBoxPrivateKey.Text = parts(2)
+            TextBoxFilePath.Text = parts(3)
+        End If
+    End Sub
+
+    Private Sub ShowCurrentSettingValue(host As String, username As String, privateKey As String, filePath As String)
+        Try
+            ' Δημιουργία του PrivateKeyFile από το string
+            Using privateKeyStream As New IO.MemoryStream(System.Text.Encoding.UTF8.GetBytes(privateKey))
+                Dim privateKeyFile As New PrivateKeyFile(privateKeyStream)
+                Dim keyFiles As PrivateKeyFile() = {privateKeyFile}
+                Dim methods As AuthenticationMethod() = {New PrivateKeyAuthenticationMethod(username, keyFiles)}
+
+                ' Ρυθμίσεις σύνδεσης
+                Dim connectionInfo As New ConnectionInfo(host, 22, username, methods)
+
+                ' Σύνδεση μέσω SSH
+                Using client As New SshClient(connectionInfo)
+                    client.Connect()
+
+                    ' Ανάγνωση του αρχείου wp-config.php
+                    Dim commandRead As SshCommand = client.CreateCommand($"cat {filePath}")
+                    Dim fileContent As String = commandRead.Execute()
+
+                    ' Έλεγχος της τρέχουσας τιμής του AIOS_DISABLE_LOGIN_WHITELIST
+                    If fileContent.Contains("define('AIOS_DISABLE_LOGIN_WHITELIST', false);") Then
+                        LabelCurrentValue.Text = "Η τρέχουσα τιμή είναι: false"
+                    ElseIf fileContent.Contains("define('AIOS_DISABLE_LOGIN_WHITELIST', true);") Then
+                        LabelCurrentValue.Text = "Η τρέχουσα τιμή είναι: true"
+                    Else
+                        LabelCurrentValue.Text = "Η τρέχουσα τιμή δεν βρέθηκε."
+                    End If
+
+                    client.Disconnect()
+                End Using
+            End Using
+        Catch ex As Exception
+            LabelCurrentValue.Text = "Σφάλμα κατά την ανάγνωση της τρέχουσας τιμής."
+        End Try
+    End Sub
+
+    Private Sub ModifyWpConfig(host As String, username As String, privateKey As String, filePath As String)
+        ' Δημιουργία του PrivateKeyFile από το string
+        Using privateKeyStream As New IO.MemoryStream(System.Text.Encoding.UTF8.GetBytes(privateKey))
+            Dim privateKeyFile As New PrivateKeyFile(privateKeyStream)
+            Dim keyFiles As PrivateKeyFile() = {privateKeyFile}
+            Dim methods As AuthenticationMethod() = {New PrivateKeyAuthenticationMethod(username, keyFiles)}
+
+            ' Ρυθμίσεις σύνδεσης
+            Dim connectionInfo As New ConnectionInfo(host, 22, username, methods)
+
+            ' Σύνδεση μέσω SSH
+            Using client As New SshClient(connectionInfo)
+                client.Connect()
+
+                ' Ανάγνωση του αρχείου wp-config.php
+                Dim commandRead As SshCommand = client.CreateCommand($"cat {filePath}")
+                Dim fileContent As String = commandRead.Execute()
+
+                ' Τροποποίηση του περιεχομένου
+                If fileContent.Contains("define('AIOS_DISABLE_LOGIN_WHITELIST', false);") Then
+                    fileContent = fileContent.Replace("define('AIOS_DISABLE_LOGIN_WHITELIST', false);", "define('AIOS_DISABLE_LOGIN_WHITELIST', true);")
+                ElseIf fileContent.Contains("define('AIOS_DISABLE_LOGIN_WHITELIST', true);") Then
+                    fileContent = fileContent.Replace("define('AIOS_DISABLE_LOGIN_WHITELIST', true);", "define('AIOS_DISABLE_LOGIN_WHITELIST', false);")
+                Else
+                    Throw New Exception("Η συγκεκριμένη γραμμή δεν βρέθηκε στο αρχείο.")
+                End If
+
+                ' Αποθήκευση του τροποποιημένου περιεχομένου σε προσωρινό αρχείο και μεταφόρτωση
+                Dim tempLocalFilePath As String = Path.GetTempFileName()
+                File.WriteAllText(tempLocalFilePath, fileContent)
+
+                Using sftp As New SftpClient(connectionInfo)
+                    sftp.Connect()
+                    Using fileStream As New FileStream(tempLocalFilePath, FileMode.Open)
+                        sftp.UploadFile(fileStream, filePath, True)
+                    End Using
+                    sftp.Disconnect()
+                End Using
+
+                File.Delete(tempLocalFilePath)
+                client.Disconnect()
+            End Using
+        End Using
+    End Sub
+End Class
